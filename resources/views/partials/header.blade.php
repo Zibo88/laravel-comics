3e6b286fa199ed8335f369f7cbdcb3a2034a218e
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-24 16:31:58 +0200

    header-top done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,22 @@
 {{-- porzione di codice relativo all'header --}}
 <header>
-    <div class="logo"> 
+
+
+    
+    <div class="container"> 
+        <div class="header-top">
+            <div class="header-text">
+                <span>dc power visa</span> 
+                <span>additional dc site</span>
+                <i class="fa-solid fa-caret-down"></i>
+            </div>
+
+        </div>
         {{-- aggiungo immagine all'header --}}
-        <img src="{{asset('img/dc-logo.png')}}" alt="dc-logo">
+        <img class="logo" src="{{asset('img/dc-logo.png')}}" alt="dc-logo">
     </div>
 
     <div>
-        menu
+        
     </div>
 </header>
