1d326c16f62e557bd280b5bab5c70c7f6bb2a8bd
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-24 21:19:46 +0200

    footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,155 @@
 {{-- porzione di codice relativo al footer --}}
 <footer>
-    sono il footer
+    <div class="container">
+        <div class="footer flex">
+            <div class="col">
+                <ul>
+                    <h4>DC COMICS</h4>
+                    <li>
+                        <a href="">Characters</a>
+                    </li>
+
+                    <li>
+                        <a href="">Comics</a>
+                    </li>
+
+                    <li>
+                        <a href="">Movies</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">Game</a>
+                    </li>
+                    <li>
+                        <a href="">Videos</a>
+                    </li>
+                    <li>
+                        <a href="">News</a>
+                    </li>
+                </ul>
+                <!-- 1.2 lista -->
+                <ul>
+                    <h4>SHOP</h4>
+                    <li>
+                        <a href="">Shop DC</a>
+                    </li>
+
+                    <li>
+                        <a href="">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- seconda colonna -->
+            <div class="col">
+                 <ul>
+                    <h4>DC</h4>
+                    <li>
+                        <a href="">Term Of Use</a>
+                    </li>
+
+                    <li>
+                        <a href="">Privacy Police (new)</a>
+                    </li>
+
+                    <li>
+                        <a href="">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="">Ad Avertising</a>
+                    </li>
+                    <li>
+                        <a href="">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="">Talent WorkShop</a>
+                    </li>
+                    <li>
+                        <a href="">CPSC Certificates</a>
+                    </li>
+               
+                    <li>
+                        <a href="">Shop Help</a>
+                    </li>
+
+                    <li>
+                        <a href="">Contact us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- terza colonna -->
+            <div class="col">
+                <ul>
+                    <h4>SITES</h4>
+                    <li>
+                        <a href="">DC</a>
+                    </li>
+
+                    <li>
+                        <a href="">MAD Megazine</a>
+                    </li>
+
+                    <li>
+                        <a href="">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="">DC Power Visa</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- quarta colonna -->
+             <div class="footer-img">
+              <img class="footer-logo" src="{{asset("img/dc-logo-bg.png")}}" alt="logo dc">
+            </div>
+        </div>
+
+        <div class="footer-bottom flex">
+            <button >
+                <a class="btn" href="#">SIGN UP NOW!</a>
+                
+                </button>
+
+            <div class="footer-icon flex">
+                <h3>FOLLOW US</h3>
+                <ul class="social-list flex">
+                    <li>
+                        <a href="#">
+                             <img class="social" src="{{asset('img/footer-facebook.png')}}" alt="facebook logo">
+                        </a>
+                    </li>
+
+                   <li>
+                        <a href="#">
+                             <img class="social" src="{{asset('img/footer-periscope.png')}}" alt="periscope logo">
+                        </a>
+                    </li>
+
+                   <li>
+                        <a href="#">
+                             <img class="social" src="{{asset('img/footer-pinterest.png')}}" alt="pinterest logo">
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                             <img class="social" src="{{asset('img/footer-twitter.png')}}" alt="twitter logo">
+                        </a>
+                    </li>
+
+                   <li>
+                        <a href="#">
+                             <img class="social" src="{{asset('img/footer-youtube.png')}}" alt="youtube logo">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
